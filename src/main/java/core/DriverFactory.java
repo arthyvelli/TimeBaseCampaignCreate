ce8dc3f7d2fefd385e5d83339764f71e56151555
arthyvelli <48116111+arthyvelli@users.noreply.github.com>
2021-01-21 19:17:11 +0530

    Update DriverFactory.java
    semi colon missed

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeOption
+import org.openqa.selenium.chrome.ChromeOption;
 
 
 public class DriverFactory {
