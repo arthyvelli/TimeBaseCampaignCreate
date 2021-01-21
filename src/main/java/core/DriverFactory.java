6a042b0926c36b4297d4894d6ded5a3a09d8a2d9
arthyvelli <48116111+arthyvelli@users.noreply.github.com>
2021-01-21 19:14:13 +0530

    Update DriverFactory.java
    added webdriver manager

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -3,9 +3,10 @@ package core;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOption
 
 
 public class DriverFactory {
